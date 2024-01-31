8e57bf97b753b2aee57c581b979e1d6cc08d5a8d
Jac <39693487+lijac@users.noreply.github.com>
2024-01-30 19:34:14 -0500

    v0.4 - add sect

--- common/config.ts
+++ common/config.ts
@@ -3,13 +3,13 @@ export const configs = {
   linea: {
     WNATIVE: '0xe5D7C2a44FfDDf6b295A15c148167daaAf5Cf34f',
     nativeCurrencyLabel: 'ETH',
-    cake: '0x152649eA73beAb28c5b49B26eb48f7EAD6d4c898',
+    cake: '0x92ae51D09765f653B3433Df42d5dba1dde0ab039',
     smartRouterHelper: '0xdAecee3C08e953Bd5f89A5Cc90ac560413d709E3',
   },
   lineaGoerli: {
     WNATIVE: '0x2C1b868d6596a18e32E61B901E4060C872647b6C',
     nativeCurrencyLabel: 'GOR',
-    cake: '0xc2C3eAbE0368a2Ea97f485b03D1098cdD7d0c081',
+    cake: '0x92ae51D09765f653B3433Df42d5dba1dde0ab039',
     smartRouterHelper: '0xdAecee3C08e953Bd5f89A5Cc90ac560413d709E3',
   },
   hardhat: {
