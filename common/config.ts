a381b4df321408ea3531198ff2938ec35774f6ec
Jac <39693487+lijac@users.noreply.github.com>
2024-02-05 23:31:23 -0500

    v0.9.0 - tidy up

--- common/config.ts
+++ common/config.ts
@@ -6,18 +6,18 @@ export const configs = {
     WNATIVE: '0xe5D7C2a44FfDDf6b295A15c148167daaAf5Cf34f',
     nativeCurrencyLabel: 'ETH',
     cake: '0x92ae51D09765f653B3433Df42d5dba1dde0ab039',
-    smartRouterHelper: '0xdAecee3C08e953Bd5f89A5Cc90ac560413d709E3',
+    smartRouterHelper: '0x4C35fA1249a72190fc2e08fd778d0E15E8bcb586',
   },
   lineaGoerli: {
     WNATIVE: '0x2C1b868d6596a18e32E61B901E4060C872647b6C',
     nativeCurrencyLabel: 'GOR',
     cake: '0x92ae51D09765f653B3433Df42d5dba1dde0ab039',
-    smartRouterHelper: '0xdAecee3C08e953Bd5f89A5Cc90ac560413d709E3',
+    smartRouterHelper: '0x4C35fA1249a72190fc2e08fd778d0E15E8bcb586',
   },
   hardhat: {
     WNATIVE: '0x0000000000000000000000000000000000000000',
     nativeCurrencyLabel: 'ETH',
     cake: '0x0000000000000000000000000000000000000000',
-    smartRouterHelper: '0xdAecee3C08e953Bd5f89A5Cc90ac560413d709E3',
+    smartRouterHelper: '0x4C35fA1249a72190fc2e08fd778d0E15E8bcb586',
   },
 } as const
